522d7990c0cab297dc5be62f707b1745027bd064
Bharathimani <30790662+Bharathimani@users.noreply.github.com>
2017-08-18 12:36:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.get('/', function (req, res) {
 app.get('/articleone',function(req,res){
     res.send('article one is requested');
 });
+app.get('/articletwo',function(req,res){
+    res.send('article two is requested');
+});
+app.get('/articlethree',function(req,res){
+    res.send('article three is requested');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
